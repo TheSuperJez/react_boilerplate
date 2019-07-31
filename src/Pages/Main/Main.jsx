3eca8ed76ef7c98e54af7f49570e4e5ea3c79f89
rglobant <rafael.garduno@globant.com>
2019-07-31 11:38:10 -0500

    Boostrap and reactstrap

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -1,11 +1,13 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import { Button } from 'reactstrap';
 import { Welcome } from '../../components';
 
 export default function Main() {
   return (
     <React.Fragment>
       <Welcome />
+      <button type="button" className="btn btn-primary">Primary</button>
+      <Button color="primary">primary</Button>
     </React.Fragment>
   );
 }
